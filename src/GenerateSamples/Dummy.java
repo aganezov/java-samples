66f11726fd8872dd8ed3e4983f09b50b1553c44c
aganezov <aganezov@gwu.edu>
2015-10-17 10:19:47 -0400

    generate constructor

---
+++ src/GenerateSamples/Dummy.java
@@ -0,0 +1,18 @@
+package GenerateSamples;
+
+/**
+ * Created by aganezov on 10/17/15.
+ * JetBrains GWU
+ */
+public class Dummy {
+    private int field1;
+    private int field2;
+    private String field3;
+
+    /* Automatically generate by    Cmd + N -> Constructor      */
+    public Dummy(int field1, int field2, String field3) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+    }
+}
