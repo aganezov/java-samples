77fde016fa3d21ea321e237e8ac491e08c8aea6a
aganezov <aganezov@gwu.edu>
2015-10-17 10:41:30 -0400

    generate, Override Methods

---
+++ src/GenerateSamples/DummyAncestor.java
@@ -0,0 +1,11 @@
+package GenerateSamples;
+
+/**
+ * Created by aganezov on 10/17/15.
+ * JetBrains GWU
+ */
+public class DummyAncestor {
+    public void makeNoise(){
+        System.out.println("NOOOOISE");
+    }
+}
