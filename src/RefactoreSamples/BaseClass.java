264be4dec4737c9152dde18153beadca240a7ebb
aganezov <aganezov@gwu.edu>
2015-10-21 20:31:32 -0400

    refactoring, encapsulate fields

--- src/RefactoreSamples/BaseClass.java
+++ src/RefactoreSamples/BaseClass.java
@@ -5,6 +5,26 @@ package RefactoreSamples;
  * JetBrains GWU
  */
 public class BaseClass extends SuperClass{
-    public int field1;
-    public int field2;
+    private int field1;
+    private int field2;
+
+    /* Generate by Ctrl + T -> Encapsulate Fields */
+    public int getField1() {
+        return field1;
+    }
+
+    /* Generate by Ctrl + T -> Encapsulate Fields */
+    public void setField1(int field1) {
+        this.field1 = field1;
+    }
+
+    /* Generate by Ctrl + T -> Encapsulate Fields */
+    public int getField2() {
+        return field2;
+    }
+
+    /* Generate by Ctrl + T -> Encapsulate Fields */
+    public void setField2(int field2) {
+        this.field2 = field2;
+    }
 }
